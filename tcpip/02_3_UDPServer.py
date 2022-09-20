51be47adb686312b0af2f51ae06ecd810db4dda4
MerakMin <ssmin413@gmail.com>
2022-09-20 17:47:49 +0900

    Network Study commit

--- tcpip/02_3_UDPServer.py
+++ tcpip/02_3_UDPServer.py
@@ -4,7 +4,7 @@ s_ip = 'localhost'
 s_port = 12345
 s_addr = (s_ip, s_port)
 
-s_sock = socket(AF_INET, SOCK_STREAM)
+s_sock = socket(AF_INET, SOCK_DGRAM)
 s_sock.bind((s_ip, s_port))
 
 s_sock.settimeout(5)
