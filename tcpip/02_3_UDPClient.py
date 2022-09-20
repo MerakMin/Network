6fa34eb0a6c3de4321668c9a861da09b1cff7763
MerakMin <ssmin413@gmail.com>
2022-09-20 17:41:51 +0900

    Network Study commit

--- tcpip/02_3_UDPClient.py
+++ tcpip/02_3_UDPClient.py
@@ -4,7 +4,7 @@ s_ip = 'localhost'
 s_port = 12345
 c_addr = (s_ip, s_port)
 
-c_sock = socket(AF_INET, SOCK_STREAM)
+c_sock = socket(AF_INET, SOCK_DGRAM)
 
 c_sock.sendto('안녕, server!'.encode('utf-8'), (s_ip, s_port))
 
